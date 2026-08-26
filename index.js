59cf34164c958173f707777bcf3d2b8e437c90a1
Guilherme Moraes Rodrigues <161168670+GuilhermeMoraes12bTI@users.noreply.github.com>
2026-08-26 11:47:55 -0300

    Atualizando o projeto
    Adicionando HTML e CSS

--- index.js
+++ index.js
@@ -1,80 +1,85 @@
-import express from "express";
-import mysql2 from "mysql2";
+import express from "express"
+import mysql2 from "mysql2"
+import cors from "cors"
 
-const app = express();
-app.use(express.json());
+const app = express()
 
+app.use(express.json())
+app.use(cors())
 
+app.get("/", (request, response) => {
+    const selectCommand = "SELECT * FROM filmes_guilhermemoraes"
 
-app.get ("/", (request, response)=>{
-    response.json({
-        message:"Servidor dos filmes aberto"
+    sql.query(selectCommand, (error, data) => {
+        if (error) {
+            console.log(error)
+            return
+        }
+
+        response.json(data)
+    })
+})
+
+app.post("/create", (request, response) => {
+    const { title, gender, ageLimit, duration } = request.body
+
+    const insertCommand = "INSERT INTO filmes_guilhermemoraes(title, genre, age_rating, duration) VALUES (?, ?, ?, ?)"
+
+    sql.query(insertCommand, [title, genre, age_rating, duration], (error) => {
+        if(error) {
+            console.log(error)
+            return
+        }
+
+        response.status(201).json({
+            message: "Filme cadastrado com sucesso!"
+        })
+    })
+})
+
+app.delete("/delete/:id", (request, response) => {
+    const { id } = request.params
+
+    const deleteCommand = "DELETE FROM correcao_MarcioMarcal WHERE id=?"
+
+    sql.query(deleteCommand, [id], (error) => {
+        if (error) {
+            console.log(error)
+            return
+        }
+
+        response.json({
+            message: "Filme apagado com sucesso!"
+        })
+    })
+})
+
+app.put("/update/:id", (request, response) => {
+    const { id } = request.params
+    const { title, gender, ageLimit, duration } = request.body
+
+    let updateCommand = "UPDATE correcao_MarcioMarcal SET title = ?, genre = ?, age_rating = ?, duration = ? WHERE id = ?"
+
+    sql.query(updateCommand, [title, gender, ageLimit, duration, id], (error) => {
+        if (error) {
+            console.log(error)
+            return
+        }
+
+        response.json({
+            message: "Filme alterado com sucesso!"
+        })
     })
 })
 
 
-app.post("/filmes", (request, response) => {
-    const { id, title, genre, duration, age_rating } = request.body;
-    
-    const insertCommand = "INSERT INTO filmes_guilhermemoraes (id, title, genre, duration, age_rating) VALUES (?, ?, ?, ?, ?)";
-
-    sql.query(insertCommand, [id, title, genre, duration, age_rating], (error) => {
-        if (error) {
-            console.log(error);
-            return response.status(500).json({ error: "Erro interno ao adicionar o filme." });
-        }
-        
-        response.status(201).json({
-            message: "Filme adicionado com sucesso!"
-        });
-    });
-});
-
-
-app.put("/filmes/:id", (request, response) => {
-    const { id } = request.params; 
-    const { title, genre, duration, age_rating } = request.body; 
-
-    const updateCommand = "UPDATE filmes_guilhermemoraes SET title=?, genre=?, duration=?, age_rating=? WHERE id=?";
-
-    sql.query(updateCommand, [title, genre, duration, age_rating, id], (error) => {
-        if (error) {
-            console.log(error);
-            return response.status(500).json({ error: "Erro interno ao atualizar o filme." });
-        }
-
-        response.status(200).json({
-            message: "Filme atualizado com sucesso!"
-        });
-    });
-});
-
-
-app.delete("/filmes/:id", (request, response) => {
-    const { id } = request.params; 
-
-    const deleteCommand = "DELETE FROM filmes_guilhermemoraes WHERE id=?";
-
-    sql.query(deleteCommand, [id], (error) => {
-        if (error) {
-            console.log(error);
-            return response.status(500).json({ error: "Erro interno ao deletar o filme." });
-        }
-        
-        response.status(200).json({
-            message: "Filme apagado com sucesso!"
-        });
-    });
-});
-
 app.listen(3000, () => {
-    console.log("Servidor aberto na porta 3000");
-});
-
+    console.log("Servidor online")
+})
 
 const sql = mysql2.createPool({
     host: "benserverplex.ddns.net",
     user: "alunos",
     password: "senhaAlunos",
-    database: "alunos_filmes03MB" 
-});
+    database: "alunos_filmes03MB"
+})
